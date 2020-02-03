b538b4ae2acf155e74e8af66ec889c681327de0a
Jeny <853297695@qq.com>
2020-02-03 14:58:51 +0800

    完善登录页面的样式

--- lib/pages/user/login.dart
+++ lib/pages/user/login.dart
@@ -14,40 +14,50 @@ class _LoginPageState extends State<LoginPage> {
       appBar: AppBar(
         title: Text('登录'),
       ),
-      body: Column(
-        children: <Widget>[
-          TextField(
-            decoration: InputDecoration(labelText: '用户名', hintText: '请输入用户名'),
-          ),
-          TextField(
-            obscureText: showPassword,
-            decoration: InputDecoration(
-              labelText: '密码', 
-              hintText: '请输入密码',
-              suffixIcon: IconButton(icon: Icon(showPassword?Icons.visibility_off:Icons.visibility), onPressed: () {
-                setState(() {
-                  showPassword = !showPassword;
-                });
-              })
-              ),
-          ),
-          RaisedButton(child: Text('登录'), onPressed: () {
-            print('登录');
-          }),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('还没账号？'),
-              FlatButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, 'register');
-                },
-                child: Text('去注册~'),
-                textColor: Colors.blue,
-              )
-            ],
-          )
-        ],
+      body: SafeArea(
+        minimum: EdgeInsets.all(30.0),
+              child: ListView(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: '用户名', hintText: '请输入用户名'),
+            ), 
+            Padding(padding: EdgeInsets.all(10.0),),
+            TextField(
+              obscureText: showPassword,
+              decoration: InputDecoration(
+                labelText: '密码', 
+                hintText: '请输入密码',
+                suffixIcon: IconButton(icon: Icon(showPassword?Icons.visibility_off:Icons.visibility), onPressed: () {
+                  setState(() {
+                    showPassword = !showPassword;
+                  });
+                })
+                ),
+            ),
+           Padding(padding: EdgeInsets.all(10.0),),
+            RaisedButton(
+              child: Text('登录'), 
+              color: Colors.green,
+              textColor: Colors.white,
+            onPressed: () {
+              print('登录');
+            }),
+            Padding(padding: EdgeInsets.all(10.0),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text('还没账号？'),
+                FlatButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, 'register');
+                  },
+                  child: Text('去注册~'),
+                  textColor: Colors.green,
+                )
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
